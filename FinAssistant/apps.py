3d5f86ae6c348ba47f9f3ed9f68ad751ef76ad8b
NakoDS <nazarenkoandrew@gmail.com>
2019-02-12 01:01:23 +0300

    #Issue 2: Создать нормальный чистый jango проект Удален старый проект, создан новый. В репозиторий так же занесены venv и manage.py (может нужно будет удалить)

--- FinAssistant/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FinassistantConfig(AppConfig):
-    name = 'FinAssistant'
--- FinAssistant/wsgi.py
+++ FinAssistant/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for untitled1 project.
+WSGI config for FinAssistant project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FinAssistant.settings')
 
 application = get_wsgi_application()
-dsfsdfsdfsdf
